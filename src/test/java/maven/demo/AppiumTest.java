9e750e7b0cc0ec7c322c73b1311e0f38f0edbaac
Anand Kumar <anandkmr740@gmail.com>
2022-08-30 12:38:22 +0530

    first commit

---
+++ src/test/java/maven/demo/AppiumTest.java
@@ -0,0 +1,14 @@
+package maven.demo;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeApp() {
+		System.out.println("Native App");
+	}
+	@Test
+	public void iosAppAndroid() {
+		System.out.println("ios Android");
+	}
+}
---
+++ src/test/java/maven/demo/SeleniumTest.java
@@ -0,0 +1,17 @@
+package maven.demo;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+
+	@Test
+	public void browserAutomation() {
+		System.out.println("Browser Automation");
+	}
+	
+	@Test
+	public void elementsUi() {
+		System.out.println("Element UI");
+	}
+}
